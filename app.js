414f962157b383535c0fc395e4631661511872fd
SkurczyByq <skurczy_byq1@proton.me>
2023-11-12 18:54:44 +0100

    Website files
    Added some website files

---
+++ app.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let button = {
+        home: document.getElementById('navbar-logo'),
+        github: document.getElementById('github-button'),
+        discord: document.getElementById('discord-button')
+    }
+
+    let link = {
+        homepage: 'index.html',
+        github: 'https://github.com/SkurczyByq1',
+        discord: null,
+    }
+
+    button.home.addEventListener('click', function() {
+        window.location.href = link.homepage;
+    })
+
+    button.github.addEventListener('click', function() {
+        window.location.href = link.github;
+    })
+
+    button.discord.addEventListener('click', function() {
+        window.location.href = link.discord;
+    })
+})
