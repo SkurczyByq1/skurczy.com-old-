d451367649ed1adc8b5f9038c8fc3ab8ca0e1e15
SkurczyByq <skurczy_byq1@proton.me>
2024-01-16 16:22:37 +0100

    Page files
    Updated JavaScript

---
+++ app/index.js
@@ -0,0 +1,25 @@
+import {links, buttons} from './main.js';
+
+console.log('Website version: Beta 1.0')
+
+document.addEventListener('DOMContentLoaded', function() {
+    buttons.navbar.addEventListener('click', function() {
+        window.location.href = links.homepage;
+    })
+
+    buttons.footer.addEventListener('click', function() {
+        window.location.href = links.homepage;
+    })
+
+    buttons.projects.addEventListener('click', function() {
+        window.location.href = links.projects;
+    })
+
+    buttons.github.addEventListener('click', function() {
+        window.location.href = links.github;
+    })
+
+    buttons.discord.addEventListener('click', function() {
+        window.location.href = links.discord;
+    })
+})
---
+++ app/main.js
@@ -0,0 +1,25 @@
+export const links = {
+    homepage: 'index.html',
+    projects: 'projects.html',
+    about: 'about.html',
+    websiteProject: 'website-project.html',
+    github: 'https://github.com/SkurczyByq1',
+    discord: '_blank',
+    x: 'https://twitter.com/SkurczyByq',
+    spotify: 'https://open.spotify.com/user/c3u1dr7k7irscm046lsdj4ooz?si=ad52d70758e344e5'
+}
+
+export const buttons = {
+    navbar: document.getElementById('navbar-logo'),
+    footer: document.getElementById('footer-logo'),
+    projects: document.getElementById('projects-button'),
+//  about: document.getElementById('about-button'),
+    websiteProject: document.getElementById('website-card-button'),
+    goBack: document.getElementById('back-to-previous'),
+    github: document.getElementById('github-button'),
+    githubPin: document.getElementById('github-pin'),
+    discord: document.getElementById('discord-button'),
+    discordPin: document.getElementById('discord-pin'),
+    xPin: document.getElementById('x-pin'),
+    spotifyPin: document.getElementById('spotify-pin')
+}
